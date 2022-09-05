0d7feee04578fabb98f8a59bb5fc88e094ca211a
Pagani Walter <paganiwalter@gmail.com>
2022-09-05 15:27:13 -0300

    Add auras 71065 and 83470 to the npc

--- data/sql/updates/pending_db_world/rev_1662388517686257200.sql
+++ data/sql/updates/pending_db_world/rev_1662388517686257200.sql
@@ -4,3 +4,7 @@ INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Fligh
 (44794, 0, 0, 0, 1, 0, 0);
 
 UPDATE `creature` SET `spawndist`=0, `MovementType`=0 WHERE `guid`=251604;
+
+DELETE FROM `creature_template_addon` WHERE `entry`=38038;
+INSERT INTO `creature_template_addon` (`entry`, `waypointPathId`, `cyclicSplinePathId`, `mount`, `bytes1`, `bytes2`, `emote`, `aiAnimKit`, `movementAnimKit`, `meleeAnimKit`, `visibilityDistanceType`, `auras`) VALUES
+(38038, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '71065 83470');
