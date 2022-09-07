1e6dc6e5b3cefcf9330ed5333eaaf43d3c76f185
{DEV}_Bruenoor <rubendgg84@gmail.com>
2022-09-07 01:00:50 +0100

    Update 2022_09_06_002_world_npc_geezle.sql

--- data/sql/updates/pending_db_world/2022_09_06_002_world_npc_geezle.sql
+++ data/sql/updates/pending_db_world/2022_09_06_002_world_npc_geezle.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET  AIName= '', `ScriptName`= 'npc_geezle' WHERE  `entry`= 17318;
+UPDATE `creature_template` SET AIName= '', `ScriptName`= 'npc_geezle' WHERE  `entry`= 17318;
