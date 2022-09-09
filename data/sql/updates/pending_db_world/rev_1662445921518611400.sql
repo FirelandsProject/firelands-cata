28c9c71ee5333532d9c69667b5b54fb78b00beef
Walter Pagani <paganiwalter@gmail.com>
2022-09-09 11:33:33 -0300

    Line break at the end of the document
    The line break is added and the values of 0 are changed to NULL. Since at the moment, sometimes the 0 fails. But the NULL works and since you just want to enable the flight, NULL would be fine anyway.

--- data/sql/updates/pending_db_world/rev_1662445921518611400.sql
+++ data/sql/updates/pending_db_world/rev_1662445921518611400.sql
@@ -1,5 +1,5 @@
-DELETE FROM `creature_template_movement` WHERE `CreatureId` in (50372,49044,49128);
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (50372, 49044, 49128);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Random`, `InteractionPauseTimer`) VALUES 
-(50372, 0, 0, 1, 0, 0, NULL),
-(49044, 0, 0, 1, 0, 0, NULL),
-(49128, 0, 0, 1, 0, 0, NULL);
+(50372, NULL, NULL, 1, NULL, NULL, NULL),
+(49044, NULL, NULL, 1, NULL, NULL, NULL),
+(49128, NULL, NULL, 1, NULL, NULL, NULL);
