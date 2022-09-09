fb887b2fb7a660d9767f813dc8a4d67d44e79c66
Walter Pagani <paganiwalter@gmail.com>
2022-09-09 12:26:07 -0300

    Capitalized reserved words

--- data/sql/updates/pending_db_world/rev_1662505989561096200.sql
+++ data/sql/updates/pending_db_world/rev_1662505989561096200.sql
@@ -1,4 +1,4 @@
-update `gameobject_template` set `AIName`= 'SmartGameObjectAI' where `entry`= 181148;
+UPDATE `gameobject_template` SET `AIName`='SmartGameObjectAI' WHERE `entry`=181148;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=181148 AND `source_type`=1;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
