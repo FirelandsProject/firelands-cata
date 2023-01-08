7ab66c76ca1995bac65d40e299db95f3c2565e10
M'Dic <joshua.lee.betts@gmail.com>
2023-01-08 11:25:26 -0500

    fix (false hit): teleport false hit fix
    removed z check for teleport check as it caused false hits with hearth stoning

--- src/server/game/Anticheat/AnticheatMgr.cpp
+++ src/server/game/Anticheat/AnticheatMgr.cpp
@@ -518,12 +518,8 @@ void AnticheatMgr::TeleportHackDetection(Player* player, MovementInfo movementIn
     float lastY = m_Players[key].GetLastMovementInfo().pos.GetPositionY();
     float newY = movementInfo.pos.GetPositionY();
 
-    float lastZ = m_Players[key].GetLastMovementInfo().pos.GetPositionZ();
-    float newZ = movementInfo.pos.GetPositionZ();
-
     float xDiff = fabs(lastX - newX);
     float yDiff = fabs(lastY - newY);
-    float zDiff = fabs(lastZ - newZ);
 
     if (player->IsFalling() || (player->IsFalling() && player->IsMounted()))
         return;
@@ -531,7 +527,7 @@ void AnticheatMgr::TeleportHackDetection(Player* player, MovementInfo movementIn
     /* Dueling exploit detection*/
     if (player->duel)
     {
-        if ((xDiff >= 50.0f || yDiff >= 50.0f || (zDiff >= 10.0f && !player->IsFlying())) && !player->CanTeleport())
+        if ((xDiff >= 50.0f || yDiff >= 50.0f) && !player->CanTeleport())
         {
             Player* opponent = player->duel->opponent;
 
@@ -559,7 +555,7 @@ void AnticheatMgr::TeleportHackDetection(Player* player, MovementInfo movementIn
             player->SetCanTeleport(false);
     }
     /* Please work */
-    if ((xDiff >= 50.0f || yDiff >= 50.0f || (zDiff >= 10.0f && !player->IsFlying())) && !player->CanTeleport())// teleport helpers in play
+    if ((xDiff >= 50.0f || yDiff >= 50.0f) && !player->CanTeleport())// teleport helpers in play
     {
         if (m_Players[key].GetTotalReports() > sWorld->getIntConfig(CONFIG_ANTICHEAT_REPORTS_INGAME_NOTIFICATION))
         {// we do this because we can not get the collumn count being propper when we add more collumns for the report, so we make a indvidual warning for Teleport Hack
