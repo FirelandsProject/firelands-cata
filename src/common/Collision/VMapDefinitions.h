2b488e2cb5302a631ed2cb07d47d106f76606d32
Jose Joya <ing.jose.joya@gmail.com>
2023-07-27 00:30:30 -0500

    feat: Added Maps Compatibility with another Cores (#113)

--- src/common/Collision/VMapDefinitions.h
+++ src/common/Collision/VMapDefinitions.h
@@ -17,20 +17,20 @@
 
 #ifndef _VMAPDEFINITIONS_H
 #define _VMAPDEFINITIONS_H
-#include <cstring>
 #include <cstdio>
+#include <cstring>
 
 #define LIQUID_TILE_SIZE (533.333f / 128.f)
 
 namespace VMAP
 {
-    const char VMAP_MAGIC[] = "VMAP_4.8";
-    const char RAW_VMAP_MAGIC[] = "VMAP048";                // used in extracted vmap files with raw data
-    const char GAMEOBJECT_MODELS[] = "GameObjectModels.dtree";
+const char VMAP_MAGIC[] = "VMAP_4.4";
+const char RAW_VMAP_MAGIC[] = "VMAP044"; // used in extracted vmap files with raw data
+const char GAMEOBJECT_MODELS[] = "GameObjectModels.dtree";
 
-    // defined in TileAssembler.cpp currently...
-    bool readChunk(FILE* rf, char *dest, const char *compare, uint32 len);
-}
+// defined in TileAssembler.cpp currently...
+bool readChunk(FILE *rf, char *dest, const char *compare, uint32 len);
+} // namespace VMAP
 
 // Set of helper macros for extractors (VMAP and MMAP)
 #ifndef NO_CORE_FUNCS
@@ -40,7 +40,7 @@ namespace VMAP
 #else
 #define VMAP_ERROR_LOG(FILTER, ...) (void)sizeof(FILTER)
 #define VMAP_DEBUG_LOG(FILTER, ...) (void)sizeof(FILTER)
-#define VMAP_INFO_LOG(FILTER, ...)  (void)sizeof(FILTER)
+#define VMAP_INFO_LOG(FILTER, ...) (void)sizeof(FILTER)
 #endif
 
 #endif
