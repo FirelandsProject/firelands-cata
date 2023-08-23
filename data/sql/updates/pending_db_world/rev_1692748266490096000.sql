07349f047f56253bef1bdbce90cbd7ea7b5fe5b6
M'Dic <joshua.lee.betts@gmail.com>
2023-08-22 20:22:57 -0400

    Update rev_1692748266490096000.sql

--- data/sql/updates/pending_db_world/rev_1692748266490096000.sql
+++ data/sql/updates/pending_db_world/rev_1692748266490096000.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `lua_private_functions`;
 CREATE TABLE IF NOT EXISTS `lua_private_functions` (
-  `function_name` text NOT NULL,
-  `enabled` TINYINT UNSIGNED NOT NULL DEFAULT '1',
+  `function_name` varchar(255) NOT NULL DEFAULT '0',
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`function_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
