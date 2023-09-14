38a4ff7190a2dc70155f976d43a46bced1cdce86
Jose Joya (seobryn) <ing.jose.joya@gmail.com>
2023-09-14 09:42:55 -0500

    feat: adding rbac permissions when set gm level

--- src/server/game/Accounts/AccountMgr.h
+++ src/server/game/Accounts/AccountMgr.h
@@ -20,85 +20,81 @@
 #define _ACCMGR_H
 
 #include "RBAC.h"
+#include "Common.h"
 
-enum class AccountOpResult : uint8 {
-  AOR_OK,
-  AOR_NAME_TOO_LONG,
-  AOR_PASS_TOO_LONG,
-  AOR_EMAIL_TOO_LONG,
-  AOR_NAME_ALREADY_EXIST,
-  AOR_NAME_NOT_EXIST,
-  AOR_DB_INTERNAL_ERROR,
-  AOR_ACCOUNT_BAD_LINK
+enum class AccountOpResult : uint8
+{
+    AOR_OK,
+    AOR_NAME_TOO_LONG,
+    AOR_PASS_TOO_LONG,
+    AOR_EMAIL_TOO_LONG,
+    AOR_NAME_ALREADY_EXIST,
+    AOR_NAME_NOT_EXIST,
+    AOR_DB_INTERNAL_ERROR,
+    AOR_ACCOUNT_BAD_LINK
 };
 
-enum PasswordChangeSecurity { PW_NONE, PW_EMAIL, PW_RBAC };
+enum PasswordChangeSecurity
+{
+    PW_NONE,
+    PW_EMAIL,
+    PW_RBAC
+};
 
 #define MAX_PASS_STR 16
 #define MAX_ACCOUNT_STR 16
 #define MAX_EMAIL_STR 64
 
-namespace rbac {
-typedef std::map<uint32, rbac::RBACPermission *> RBACPermissionsContainer;
-typedef std::map<uint8, rbac::RBACPermissionContainer>
-    RBACDefaultPermissionsContainer;
+namespace rbac
+{
+    typedef std::map<uint32, rbac::RBACPermission*> RBACPermissionsContainer;
+    typedef std::map<uint8, rbac::RBACPermissionContainer> RBACDefaultPermissionsContainer;
 } // namespace rbac
 
-class FC_GAME_API AccountMgr {
-private:
-  AccountMgr();
-  ~AccountMgr();
+class FC_GAME_API AccountMgr
+{
+  private:
+    AccountMgr();
+    ~AccountMgr();
 
-public:
-  static AccountMgr *instance();
+  public:
+    static AccountMgr* instance();
 
-  AccountOpResult CreateAccount(std::string username, std::string password,
-                                std::string email = "");
-  static AccountOpResult DeleteAccount(uint32 accountId);
-  static AccountOpResult ChangeUsername(uint32 accountId,
-                                        std::string newUsername,
-                                        std::string newPassword);
-  static AccountOpResult ChangePassword(uint32 accountId,
-                                        std::string newPassword);
-  static AccountOpResult ChangeEmail(uint32 accountId, std::string newEmail);
-  static AccountOpResult ChangeRegEmail(uint32 accountId, std::string newEmail);
-  static bool CheckPassword(uint32 accountId, std::string password);
-  static bool CheckEmail(uint32 accountId, std::string newEmail);
+    AccountOpResult CreateAccount(std::string username, std::string password, std::string email = "");
+    static AccountOpResult DeleteAccount(uint32 accountId);
+    static AccountOpResult ChangeUsername(uint32 accountId, std::string newUsername, std::string newPassword);
+    static AccountOpResult ChangePassword(uint32 accountId, std::string newPassword);
+    static AccountOpResult ChangeEmail(uint32 accountId, std::string newEmail);
+    static AccountOpResult ChangeRegEmail(uint32 accountId, std::string newEmail);
+    static bool CheckPassword(uint32 accountId, std::string password);
+    static bool CheckEmail(uint32 accountId, std::string newEmail);
 
-  static uint32 GetId(std::string const &username);
-  static uint32 GetSecurity(uint32 accountId, int32 realmId);
-  [[nodiscard]] static QueryCallback
-  GetSecurityAsync(uint32 accountId, int32 realmId,
-                   std::function<void(uint32)> callback);
-  static bool GetName(uint32 accountId, std::string &name);
-  static bool GetEmail(uint32 accountId, std::string &email);
-  static uint32 GetCharactersCount(uint32 accountId);
+    static uint32 GetId(std::string const& username);
+    static uint32 GetSecurity(uint32 accountId, int32 realmId);
+    [[nodiscard]] static QueryCallback GetSecurityAsync(uint32 accountId, int32 realmId, std::function<void(uint32)> callback);
+    static bool GetName(uint32 accountId, std::string& name);
+    static bool GetEmail(uint32 accountId, std::string& email);
+    static uint32 GetCharactersCount(uint32 accountId);
 
-  static std::string CalculateShaPassHash(std::string const &name,
-                                          std::string const &password);
-  static bool IsBannedAccount(std::string const &name);
-  static bool IsPlayerAccount(uint32 gmlevel);
-  static bool IsAdminAccount(uint32 gmlevel);
-  static bool IsConsoleAccount(uint32 gmlevel);
-  static bool HasPermission(uint32 accountId, uint32 permission,
-                            uint32 realmId);
+    static std::string CalculateShaPassHash(std::string const& name, std::string const& password);
+    static bool IsBannedAccount(std::string const& name);
+    static bool IsPlayerAccount(uint32 gmlevel);
+    static bool IsAdminAccount(uint32 gmlevel);
+    static bool IsConsoleAccount(uint32 gmlevel);
+    static bool HasPermission(uint32 accountId, uint32 permission, uint32 realmId);
 
-  void UpdateAccountAccess(rbac::RBACData *rbac, uint32 accountId,
-                           uint8 securityLevel, int32 realmId);
+    void UpdateAccountAccess(rbac::RBACData* rbac, uint32 accountId, uint8 securityLevel, int32 realmId);
 
-  void LoadRBAC();
-  rbac::RBACPermission const *GetRBACPermission(uint32 permission) const;
+    void LoadRBAC();
+    rbac::RBACPermission const* GetRBACPermission(uint32 permission) const;
 
-  rbac::RBACPermissionsContainer const &GetRBACPermissionList() const {
-    return _permissions;
-  }
-  rbac::RBACPermissionContainer const &
-  GetRBACDefaultPermissions(uint8 secLevel);
+    rbac::RBACPermissionsContainer const& GetRBACPermissionList() const { return _permissions; }
+    rbac::RBACPermissionContainer const& GetRBACDefaultPermissions(uint8 secLevel);
 
-private:
-  void ClearRBAC();
-  rbac::RBACPermissionsContainer _permissions;
-  rbac::RBACDefaultPermissionsContainer _defaultPermissions;
+  private:
+    void ClearRBAC();
+    rbac::RBACPermissionsContainer _permissions;
+    rbac::RBACDefaultPermissionsContainer _defaultPermissions;
 };
 
 #define sAccountMgr AccountMgr::instance()
