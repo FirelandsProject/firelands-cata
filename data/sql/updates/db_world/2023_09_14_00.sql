5f28ff009373a62afb8ef58a2a59886f62807b75
Jose Joya (seobryn) <ing.jose.joya@gmail.com>
2023-09-18 23:44:16 -0500

    fix: updated sql sentence to avoid error in syntax for disables

--- data/sql/updates/db_world/2023_09_14_00.sql
+++ data/sql/updates/db_world/2023_09_14_00.sql
@@ -4,10 +4,10 @@ INSERT INTO command (name, permission, help) VALUES
 ('disable add item', 875, 'Syntax: .disable add item #itemId #flag'),
 ('disable remove item', 876, 'Syntax: .disable remove item #itemId');
 
---
+
 DELETE FROM disables WHERE sourceType = 9;
-INSERT INTO disables (sourceType, entry, flags, comment) VALUES
-(9,  2586, 3, 'Disable item 2586 (GameMaster\'s Robe) for Auction House Player & Bot'),
-(9, 11508, 3, 'Disable item 11508 (GameMaster\'s Slippers) for Auction House Player & Bot'),
-(9, 12064, 3, 'Disable item 12064 (GameMaster\'s Hood) for Auction House Player & Bot'),
-(9,  7192, 2, 'Disable item 7192 (Plans: Goblin Rocket Boots) for Auction House Bot');
+INSERT INTO disables (sourceType, entry, flags, comment, params_0, params_1) VALUES
+(9,  2586, 3, 'Disable item 2586 (GameMaster\'s Robe) for Auction House Player & Bot', "", ""),
+(9, 11508, 3, 'Disable item 11508 (GameMaster\'s Slippers) for Auction House Player & Bot', "", ""),
+(9, 12064, 3, 'Disable item 12064 (GameMaster\'s Hood) for Auction House Player & Bot', "", ""),
+(9,  7192, 2, 'Disable item 7192 (Plans: Goblin Rocket Boots) for Auction House Bot', "", "");
